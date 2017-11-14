545b3a5853362e329186e2fc82a23b8229dc2d3f
Romain Sartorius <romain@fewlines.co>
2017-11-14 21:05:01 +0100

    iteration

--- 02_programming_fundamentals/03_algorithms_day_1/04_play_with_for_loop/03_iteration.js
+++ 02_programming_fundamentals/03_algorithms_day_1/04_play_with_for_loop/03_iteration.js
@@ -2,3 +2,8 @@
 //
 // -  Create an array called `litteralDigits` from `zero` to `nine` where each array entry is a spelled-out number;
 // -  Using `length`, write on `stdout` each values of the table.
+const litteralDigits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+for(let i = 0; i <= (litteralDigits.length - 1); i = i + 1) {
+  console.log(litteralDigits[i]);
+};
